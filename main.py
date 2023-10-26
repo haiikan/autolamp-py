dd50c926db7b68b1885a88ac898164e57b95d006
haiikan <aldiwiraw.an060@gmail.com>
2023-10-26 15:02:17 +0700

    UPDATE 1. add initial telegram bot

--- main.py
+++ main.py
@@ -1,31 +1,45 @@
-# index.py
-import tinytuya
-import time
-# OutletDevice(deviceId, ipAddress, localKey)
-d = tinytuya.BulbDevice('03315605c4dd57038370', '192.168.0.100', "^z&u4.vFh/kQKZQh")
-d.set_version(3.3)
-# Get Status
-data = d.status() 
-print('set_status() result %r' % data)
-# Turn Off
-# d.turn_on()
+import logging
+from telegram import __version__ as TG_VER
+try:
+    from telegram import __version_info__
+except ImportError:
+    __version_info__ = (0, 0, 0, 0, 0)  # type: ignore[assignment]
 
-# d.set_colour(255,0,0) 
-# d.set_brightness(100)
+if __version_info__ < (20, 0, 0, "alpha", 1):
+    raise RuntimeError(
+        f"This example is not compatible with your current PTB version {TG_VER}. To view the "
+        f"{TG_VER} version of this example, "
+        f"visit https://docs.python-telegram-bot.org/en/v{TG_VER}/examples.html"
+    )
 
-# d.set_colourtemp(0)
-# d.set_brightness(1000)
+from telegram import ForceReply, Update, InlineKeyboardButton, InlineKeyboardMarkup
+from telegram.ext import Application, CommandHandler, ContextTypes, MessageHandler, filters, CallbackQueryHandler
+import lamp
 
-# # Cycle through the Rainbow
-# rainbow = {"red": [255, 0, 0], "orange": [255, 127, 0], "yellow": [255, 200, 0],
-#           "green": [0, 255, 0], "blue": [0, 0, 255], "indigo": [46, 43, 95],
-#           "violet": [139, 0, 255]}
-# for color in rainbow:
-#     [r, g, b] = rainbow[color]
-#     d.set_colour(r, g, b, nowait=True)  # nowait = Go fast don't wait for response
-#     time.sleep(0.25)
+# Enable logging
+logging.basicConfig(
+    format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
+)
+# set higher logging level for httpx to avoid all GET and POST requests being logged
+logging.getLogger("httpx").setLevel(logging.WARNING)
 
-d.set_value(25, '002803e8000a')
+logger = logging.getLogger(__name__)
+
+async def start(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user = update.effective_user
+    await update.message.reply_html(
+        rf"Hi {user.mention_html()}!",
+        reply_markup=ForceReply(True)
+    )
 
 
-# d.turn_off()
+
+def main():
+    application = Application.builder().token("6406570334:AAGdCr5nv8Km7ZyvtKvZuSKkiunXZwzGz4U").build()
+    # on different commands - answer in Telegram
+    application.add_handler(CommandHandler("start", start))
+    # Run the bot until the user presses Ctrl-C
+    application.run_polling(allowed_updates=Update.ALL_TYPES)
+
+if __name__ == "__main__":
+    main()
